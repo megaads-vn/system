a93f4bc53354f64db97f8abce72da5a0bb7493e5
Bach Nguyen <bachnx2303@gmail.com>
2022-11-15 14:30:24 +0700

    optimize: app port 22 to firewalld

--- block_ssh.sh
+++ block_ssh.sh
@@ -9,6 +9,8 @@ if [ $OS == 7 ]; then
     firewall-cmd --zone=work --add-service=ssh --permanent
     firewall-cmd --reload
     firewall-cmd --zone=public --remove-service=ssh --permanent
+    firewall-cmd --zone=public --remove-port=22/tcp --permanent
+    firewall-cmd --zone=public --remove-port=22/udp --permanent
     firewall-cmd --reload
 else
     iptables -A INPUT -p tcp --dport 22 -s 118.70.126.121 -j ACCEPT
