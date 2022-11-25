ec8e1eabeb67eb63774ca2f01a76ed380d26afff
bachnx23 <bachnx2303@gmail.com>
2022-11-25 14:21:49 +0700

    update firewall command

--- block_ssh.sh
+++ block_ssh.sh
@@ -3,22 +3,27 @@
 OS=$(cat /etc/*elease | rpm --eval '%{centos_ver}')
 
 if [ $OS == 7 ]; then
-    service firewalld start
-    systemctl enable firewalld
     
+    systemctl start firewalld
+    systemctl enable firewalld 
+
     firewall-cmd --permanent --add-service=http
     firewall-cmd --permanent --add-service=https
     firewall-cmd --reload
 
     firewall-cmd --zone=work --add-source=118.70.126.121/24 --permanent
     firewall-cmd --zone=work --add-source=95.111.200.151/24 --permanent
+    firewall-cmd --zone=work --add-source=188.166.226.120/24 --permanent
     firewall-cmd --reload
+
     firewall-cmd --zone=work --add-service=ssh --permanent
     firewall-cmd --reload
+
     firewall-cmd --zone=public --remove-service=ssh --permanent
     firewall-cmd --zone=public --remove-port=22/tcp --permanent
     firewall-cmd --zone=public --remove-port=22/udp --permanent
     firewall-cmd --reload
+    
 else
     iptables -A INPUT -p tcp --dport 22 -s 118.70.126.121 -j ACCEPT
     iptables -A INPUT -p tcp --dport 22 -s 95.111.200.151 -j ACCEPT
